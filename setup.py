edb351e7e0a6358a84f1c1b10aaff9c0a8cbb4f6
Arif Mehniyar <arif.mehniyar@gmail.com>
2022-04-27 02:25:11 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='efsane-fixture',
+    version='1.0.0',
+    description='Create Own Sport Fixtures',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "": ["*.xml"],
+    },
+    install_requires=[],
+    author='Arif Mehniyar',
+    author_email='arif.mehniyar@gmail.com',
+    keywords=['sport', 'fixture', 'team-fixture', 'leage-fixture', 'basketball', 'football'],
+    url='https://gitlab.com/pgdepot/mp_agent',
+    download_url='https://pypi.org/project/mp_agent/'
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
